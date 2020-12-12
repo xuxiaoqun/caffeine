19447869eb212fb0cdd1e83743f53bb462831d09
xuzhicheng <xuzhicheng@dongfangfuli.com>
2020-12-12 12:06:58 +0800

    caffeine project commit

---
+++ src/main/java/com/xzc/demo/caffeine/TestCaffeine.java
@@ -0,0 +1,13 @@
+package com.xzc.demo.caffeine;
+
+/**
+ * @author :  xuzhicheng
+ * @description :  高性能缓存caffeine case
+ * @createDate :  2020/12/12 11:56
+ */
+public class TestCaffeine {
+
+    public static void main(String[] args) {
+
+    }
+}
